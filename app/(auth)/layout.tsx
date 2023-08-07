30f429867ed0ac955c91ac96dffe2bd5a36ede4e
Ondrej89 <ondrejdudas89@gmail.com>
2023-08-07 17:03:24 +0200

    clerk possition

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       }}
     >
       <html lang='en'>
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}><div className="flex justify-center mt-20">{children}</div></body>
       </html>
     </ClerkProvider>
   );
